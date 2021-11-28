1828124d4a8e27ad3ed8925aa03bbd2042eca85f
Wingz003 <Lsanti618@gmail.com>
2021-11-28 17:24:21 -0600

    changed hide classes to hidden cards, rending of question/answers

--- script.js
+++ script.js
@@ -28,9 +28,36 @@ const questions = [
 
 const start = document.getElementById("start");
 const startBtn = document.getElementById("startBtn");
-const hide = document.querySelectorAll("hide");
 const quiz = document.getElementById("quiz"); 
 const question = document.getElementById("question");
 const timer = document.getElementById("timer");
 
 
+startBtn.addEventListener('click', startQuiz)
+
+let currentQuestion;
+
+function startQuiz() {
+    currentQuestion = 0;
+    start.setAttribute("hidden" , true);
+    question.removeAttribute("hidden");
+    renderQuestion();
+
+}
+
+
+function renderQuestion() {
+    let newQuestion = questions[currentQuestion];
+    let picks = newQuestion.choices;
+    
+    let qtitle = document.getElementById("qTitle");
+    qtitle.textContent = newQuestion.question;
+   
+    for (let i = 0; i < picks.length; i++) {
+        let currentChoice = picks[i];
+        let choiceBtn = document.getElementById("choice" + i);
+        choiceBtn.textContent = currentChoice;
+        
+    }
+   
+        };
