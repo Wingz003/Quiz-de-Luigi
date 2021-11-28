0a4ad3b1c59da496e4bb82da683b8df2a9320b57
Wingz003 <Lsanti618@gmail.com>
2021-11-28 13:31:54 -0600

    added bootstrap link, added card to start div and applied css attributes

---
+++ script.js
@@ -0,0 +1,36 @@
+const questions = [
+    {
+        question: "What is the best gaming console?",
+        choices: ["Xbox", "Playstation", "PC", "Switch"],
+        correct:  "PC"
+    },
+    {
+        question: "What is the best gaming console?",
+        choices: ["Xbox", "Playstation", "PC", "Switch"],
+        correct:  "PC"
+    },
+    {
+        question: "What is the best gaming console?",
+        choices: ["Xbox", "Playstation", "PC", "Switch"],
+        correct:  "PC"
+    },
+    {
+        question: "What is the best gaming console?",
+        choices: ["Xbox", "Playstation", "PC", "Switch"],
+        correct:  "PC"
+    },
+    {
+        question: "What is the best gaming console?",
+        choices: ["Xbox", "Playstation", "PC", "Switch"],
+        correct:  "PC"
+    },
+];
+
+const start = document.getElementById("start");
+const startBtn = document.getElementById("startBtn");
+const hide = document.querySelectorAll("hide");
+const quiz = document.getElementById("quiz"); 
+const question = document.getElementById("question");
+const timer = document.getElementById("timer");
+
+
