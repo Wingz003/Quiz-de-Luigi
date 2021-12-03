6f8c7b96b2d511971c1b02174bfa2a90e2053d9c
Wingz003 <Lsanti618@gmail.com>
2021-12-03 13:52:37 -0600

    added timer, added event target identification for choice buttons

--- script.js
+++ script.js
@@ -5,44 +5,76 @@ const questions = [
         correct:  "PC"
     },
     {
-        question: "What is the best gaming console?",
-        choices: ["Xbox", "Playstation", "PC", "Switch"],
-        correct:  "PC"
+        question: "What is the best gaming console2?",
+        choices: ["Xbox1", "Playstation1", "PC1", "Switch1"],
+        correct:  "PC1"
     },
     {
-        question: "What is the best gaming console?",
-        choices: ["Xbox", "Playstation", "PC", "Switch"],
-        correct:  "PC"
+        question: "What is the best gaming console3?",
+        choices: ["Xbox2", "Playstation2", "PC2", "Switch2"],
+        correct:  "PC2"
     },
     {
-        question: "What is the best gaming console?",
-        choices: ["Xbox", "Playstation", "PC", "Switch"],
-        correct:  "PC"
+        question: "What is the best gaming console4?",
+        choices: ["Xbox3", "Playstation3", "PC3", "Switch3"],
+        correct:  "PC3"
     },
     {
-        question: "What is the best gaming console?",
-        choices: ["Xbox", "Playstation", "PC", "Switch"],
-        correct:  "PC"
+        question: "What is the best gaming console5?",
+        choices: ["Xbox4", "Playstation4", "PC4", "Switch4"],
+        correct:  "PC4"
     },
 ];
 
-const start = document.getElementById("start");
-const startBtn = document.getElementById("startBtn");
-const quiz = document.getElementById("quiz"); 
-const question = document.getElementById("question");
-const timer = document.getElementById("timer");
-
-
-startBtn.addEventListener('click', startQuiz)
-
+let start = document.getElementById("start");
+let startBtn = document.getElementById("startBtn");
+let quiz = document.getElementById("quiz"); 
+let question = document.getElementById("question");
+let choiceList = document.getElementById("choices");
+let timer = document.getElementById("timer");
+let choiceBtn = document.querySelector(".choice-button");
+let resultBanner = document.getElementById("result-banner");
+let inputResult = document.getElementById("input-result");
+let count;
 let currentQuestion;
+let intervalID;
+
+function startCountdown() {
+    
+   
+   
+    
+    const interval = setInterval(() => {
+        
+        timer.textContent = count;
+        count--;
+        
+        console.log(count);
+        if (count < 0) {
+            clearInterval(interval);
+            console.log("ay");
+        }
+    }, 1000);
+    
+}
+
+
+
+
+
+startBtn.addEventListener('click', startQuiz);
+choiceList.addEventListener('click', nextQuestion);
+
 
 function startQuiz() {
+
     currentQuestion = 0;
     start.setAttribute("hidden" , true);
     question.removeAttribute("hidden");
     renderQuestion();
-
+    startCountdown();
+    
+    
 }
 
 
@@ -52,12 +84,53 @@ function renderQuestion() {
     
     let qtitle = document.getElementById("qTitle");
     qtitle.textContent = newQuestion.question;
-   
+    
     for (let i = 0; i < picks.length; i++) {
         let currentChoice = picks[i];
-        let choiceBtn = document.getElementById("choice" + i);
+        let choiceBtn = document.getElementById("choice" + [i]);
         choiceBtn.textContent = currentChoice;
         
+        
     }
+    
+};
+
+function correctAnswer(choiceBtn) {
+    return choiceBtn.textContent === questions[currentQuestion].correct;
+}
+
+
+function nextQuestion(event) {
    
-        };
+    let element = event.target;
+
+        if (element.matches(".choice-button")) {
+             
+               
+                 
+                 if(correctAnswer(element)) {
+                     resultBanner.removeAttribute("hidden");
+                     inputResult.textContent = "Correct!";
+                     setTimeout( () => resultBanner.setAttribute("hidden", true), 500);
+                    
+                     
+                    } else {
+                        resultBanner.removeAttribute("hidden");
+                        inputResult.textContent = "Git Gud!";
+                        setTimeout( () => resultBanner.setAttribute("hidden", true), 500);
+                        count = count - 10;
+                        timer.textContent = count;
+
+                        
+                    }
+                    
+
+               
+               currentQuestion++;
+               renderQuestion();
+        }}
+ 
+    
+    
+
+  
